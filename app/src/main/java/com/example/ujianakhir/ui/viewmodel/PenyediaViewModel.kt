6768a3960de4c0d3d2a882f3b035b7218ffcf399
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2025-01-26 10:37:03 +0700

    Tambahkan factory ViewModel untuk mengelola dependensi dalam aplikasi terapi

---
+++ app/src/main/java/com/example/ujianakhir/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.ujianakhir.ui.viewmodel
+
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ujianakhir.TerapiApplications
+import com.example.ujianakhir.ui.viewmodel.jenisterapi.HomeJenisTerapiViewModel
+import com.example.ujianakhir.ui.viewmodel.jenisterapi.InsertJenisTerapiViewModel
+import com.example.ujianakhir.ui.viewmodel.jenisterapi.UpdateJenisTerapiViewModel
+import com.example.ujianakhir.ui.viewmodel.pasienVM.DetailPasienViewModel
+import com.example.ujianakhir.ui.viewmodel.pasienVM.HomePasienViewModel
+import com.example.ujianakhir.ui.viewmodel.pasienVM.InsertPasienViewModel
+import com.example.ujianakhir.ui.viewmodel.pasienVM.UpdatePasienViewModel
+import com.example.ujianakhir.ui.viewmodel.sesiterapi.HomeSesiTerapiViewModel
+import com.example.ujianakhir.ui.viewmodel.sesiterapi.InsertSesiTerapiViewModel
+import com.example.ujianakhir.ui.viewmodel.sesiterapi.UpdateSesiTerapiViewModel
+import com.example.ujianakhir.ui.viewmodel.terapisVM.HomeTerapisViewModel
+import com.example.ujianakhir.ui.viewmodel.terapisVM.InsertTerapisViewModel
+import com.example.ujianakhir.ui.viewmodel.terapisVM.UpdateTerapisViewModel
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomePasienViewModel(aplikasiPasien().container.pasienRepository) }
+        initializer { InsertPasienViewModel(aplikasiPasien().container.pasienRepository) }
+        initializer { DetailPasienViewModel(aplikasiPasien().container.pasienRepository) }
+        initializer { UpdatePasienViewModel(createSavedStateHandle(), aplikasiPasien().container.pasienRepository) }
+
+        initializer { HomeTerapisViewModel(aplikasiPasien().container.terapisRepository) }
+        initializer { InsertTerapisViewModel(aplikasiPasien().container.terapisRepository) }
+        initializer { UpdateTerapisViewModel(createSavedStateHandle(), aplikasiPasien().container.terapisRepository) }
+
+        initializer { HomeJenisTerapiViewModel(aplikasiPasien().container.jenisTerapiRepository) }
+        initializer { InsertJenisTerapiViewModel(aplikasiPasien().container.jenisTerapiRepository) }
+        //initializer { DetailJenisTerapiViewModel(aplikasiPasien().container.jenisTerapiRepository) }
+        initializer { UpdateJenisTerapiViewModel(createSavedStateHandle(), aplikasiPasien().container.jenisTerapiRepository) }
+
+        initializer { HomeSesiTerapiViewModel(aplikasiPasien().container.sesiTerapiRepository) }
+        initializer { InsertSesiTerapiViewModel(aplikasiPasien().container.sesiTerapiRepository) }
+        initializer { UpdateSesiTerapiViewModel(createSavedStateHandle(), aplikasiPasien().container.sesiTerapiRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiPasien(): TerapiApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TerapiApplications)
